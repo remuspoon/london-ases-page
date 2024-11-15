38f1a238ace90565b471561c239f60fa059b66de
Remus Desktop <remuspoon@gmail.com>
2024-11-15 13:28:44 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './index.css';
 import StanfordLogo from './Images/stanford.png';
 import LseLogo from './Images/lse.png';
 import KingsLogo from './Images/kings.png';
-import SharanTestimonial from './Images/sharan.jpeg';
+import SharanTestimonial from './Images/sharan.jpg';
+import DorothyTestimonial from './Images/dorothy.jpg';
 import ProgramOverviewImage from './Images/DSC01598.jpg'
 import PrizeImage from './Images/DSC01766.jpg'
-import WhoImage1 from './Images/DSC01714.jpg'
+import WhoImage1 from './Images/DSC01764.jpg'
 import WhoImage2 from './Images/DSC01558.jpg'
+import ExpectImage from './Images/DSC01714.jpg'
+import Reese from './Images/reese.jpg'
+import Remus from './Images/remus.jpg'
+import Sally from './Images/sally.jpg'
+import Jyim from './Images/Jyim.jpg'
+import Brian from './Images/brian.jpg'
+import Stanley from './Images/stanley.jpg'
 
 function App() {
   return (
@@ -65,36 +73,109 @@ function App() {
       </div>
       <div className="grid grid-cols-4 gap-5 px-5 py-10 bg-white">
         <div className="col-span-4 lg:col-start-2 lg:col-end-4 flex flex-col justify-center items-center gap-5">
+        <div className="flex flex-col gap-2">
+            <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-black">What to expect</h1>
+            <p className="text-black font-roboto lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            <div className="flex justify-center items-center py-10">
+              <img src={ExpectImage} alt="Expect" className="w-full lg:w-3/4 rounded-lg drop-shadow-lg" />
+            </div>
+          </div>
           <div className="flex flex-col gap-2">
             <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-black">Dates</h1>
             <p className="text-black font-roboto lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
           </div>
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-black">What to expect</h1>
+            <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-black">Costs</h1>
             <p className="text-black font-roboto lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
           </div>
           <a href="https://google.com" target="_blank" rel="noopener noreferrer" className="bg-primary text-white p-4 rounded-md font-bold text-lg lg:text-2xl">Apply Now</a>
         </div>
       </div>
       <div className="grid grid-cols-4 gap-5 px-5 py-10 bg-primary">
-        <div className="col-span-4 lg:col-start-2 lg:col-end-4 flex flex-col justify-center items-center gap-5">
-          <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-white">Testimonials</h1>
-          <div className="flex flex-col lg:flex-row gap-10">
+        <div className="col-span-4 lg:col-start-2 lg:col-end-4 flex flex-col justify-center items-center gap-5 py-5">
+          <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk text-white pb-5">Testimonials</h1>
+          <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10">
           <div className="flex flex-col gap-2 items-center ">
-              <img src={SharanTestimonial} alt="Testimonial 1" className="rounded-full w-1/2" />
+              <a href="https://www.linkedin.com/in/dorothy-yuan/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={DorothyTestimonial} alt="Testimonial 1" className="rounded-full w-1/2" />
+              </a>
               <div className="flex flex-col px-10 items-center">
-                <p className="text-white font-roboto py-5 font-medium lg:text-lg">"The ASES Summit was transformative for me. Being exposed to another startup ecosystem and hearing from the top players in the space felt like a year’s worth of learnings compounded in a single week."</p>
-                <p className="text-white font-roboto lg:text-lg">Sharan Mansukhani</p>
-                <p className="text-white font-roboto lg:text-lg">De La Salle University</p>
+                <p className="text-white font-roboto py-5 font-medium lg:text-lg">"At the ASES Summit, I had the chance to learn directly from CEOs and venture capitalists of industry-leading companies like Tesla and Zynga. The experience of connecting with top-tier talent, many of whom have become close friends, left me feeling more inspired, supported, and empowered than ever before."</p>
+                <p className="text-white font-roboto lg:text-lg">Dorothy Yuan</p>
+                <p className="text-white font-roboto lg:text-lg">University of York, Stanford University</p>
               </div>
-            </div><div className="flex flex-col gap-2 items-center ">
+            </div>
+            <div className="flex flex-col gap-2 items-center ">
+              <a href="https://www.linkedin.com/in/sharan-mansukhani-17306a234/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
               <img src={SharanTestimonial} alt="Testimonial 1" className="rounded-full w-1/2" />
+              </a>
               <div className="flex flex-col px-10 items-center">
-                <p className="text-white font-roboto py-5 font-medium lg:text-lg">"The ASES Summit was transformative for me. Being exposed to another startup ecosystem and hearing from the top players in the space felt like a year’s worth of learnings compounded in a single week."</p>
+                <p className="text-white font-roboto py-5 font-medium lg:text-lg">"The ASES Summit was transformative for me. Being exposed to another startup ecosystem and hearing from the top players in the space felt like a year’s worth of learnings compounded in a single week. To top it off, the people at the summit are curated to give you unique perspectives on different ecosystems globally and have a great vibe overall."</p>
                 <p className="text-white font-roboto lg:text-lg">Sharan Mansukhani</p>
                 <p className="text-white font-roboto lg:text-lg">De La Salle University</p>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-5 px-5 py-10 bg-secondary">
+        <div className="col-span-4 lg:col-start-2 lg:col-end-4 flex flex-col justify-center items-center gap-5">
+          <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk py-5 text-white">Our Network Partners</h1>
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-y-10">
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/brian-donohugh/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Brian} alt="Brian" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">Brian Donohugh</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5">Co-President @ Stanford ASES 24/25</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/j-jw-yim/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Jyim} alt="Jyim" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">J Yim</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5">Co-President @ Stanford ASES 24/25</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/remuspn/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Remus} alt="Remus" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">Remus Poon</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5">Co-Director @ ASES London</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5 ">Founder @ Calmi</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/reesewong1/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Reese} alt="Reese" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">Reese Wong</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5">Chairman of LSE Entrepreneurs</p>
+              <p className="text-white font-roboto text-center lg:text-lg lg:px-10 px-5">Co-Director @ ASES London</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/sallylo079/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Sally} alt="Sally" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">Sally Lo</p>
+              <p className="text-white font-roboto text-center lg:text-lg px-5 lg:px-10">Co-President @ Kings Entrepreneurs</p>
+              <p className="text-white font-roboto text-center lg:text-lg px-5 lg:px-10">Co-Founder @EduCrate</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <a href="https://www.linkedin.com/in/stanleytanudjaja/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center">
+              <img src={Stanley} alt="Stanley" className="rounded-full w-1/2" />
+              </a>
+              <p className="text-white font-roboto lg:text-lg pt-5">Stanley Tanudjaja</p>
+              <p className="text-white font-roboto text-center lg:text-lg px-5 lg:px-10">Founder @ Giants Collective</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-5 px-5 py-10 bg-white">
+        <div className="col-span-4 lg:col-start-2 lg:col-end-4 flex flex-col justify-center items-center gap-5">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl lg:text-4xl font-semibold font-spaceGrotesk py-5">Contact</h1>
+            <p className="text-black font-roboto lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
           </div>
         </div>
       </div>
